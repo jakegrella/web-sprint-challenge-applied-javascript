a774d70834c09519019193762d3e074b92b1d4fe
Jake Grella <jgrella13@gmail.com>
2020-10-02 12:59:29 -0500

    closer to completing card

--- components/Cards.js
+++ components/Cards.js
@@ -36,54 +36,71 @@ const entry = document.querySelector('.cards-container');
 axios
 	.get('https://lambda-times-api.herokuapp.com/articles')
 	.then((res) => {
-		// console.log(res);
-		// console.log(res.data);
-		console.log(res.data.articles);
-		// console.log(res.data.articles.length);
-		// console.log(res.data.articles.bootstrap.length);
-		// console.log(res.data.articles.bootstrap[0]);
-
-		console.log(Object.keys(res.data.articles));
-
-		Object.keys(res.data.articles).forEach((elem) => {
-			console.log(Object.keys(res.data.articles)[elem]);
-		});
-
-		let obj = { first: 'hi' };
-		console.log(obj[Object.keys(obj)[0]]); //returns hi
-
-		console.log('--------------');
-
-		// res.data.articles[Object.keys(res.data.articles)[0]]
-		console.log(res.data.articles[Object.keys(res.data.articles)[0]]);
-		console.log(
-			res.data.articles[
-				Object.keys(res.data.articles)[[Object.keys(res.data.articles)[0]]]
-			]
-		);
-
-		// res.data.articles[Object.keys(res.data.articles)[0]]
-
 		let articles = res.data.articles;
-		let articleCats = [];
-		Object.keys(articles).forEach((elem) => {
-			articleCats.push(elem);
-		});
-		console.log('article titles -> ', articleCats);
+		console.log(articles);
 
-		let articleTitles = [];
-		articleCats.forEach((elem) => {
-			console.log(articles[elem]);
-			articleTitles.push(articles[elem].headline);
+		const articleCard = cardMaker({
+			headline: 'hivvfvvvvvvwsrgvr fsefgfgwe',
+			authorPhoto: '',
+			authorName: 'jake',
 		});
-		console.log('articleTitles -> ', articleTitles);
-
-		// Object.keys(articles)[0]
-		// console.log(Object.keys(articles)[0]);
+		entry.append(articleCard);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
+
+// axios
+// 	.get('https://lambda-times-api.herokuapp.com/articles')
+// 	.then((res) => {
+// 		// console.log(res);
+// 		// console.log(res.data);
+// 		console.log(res.data.articles);
+// 		// console.log(res.data.articles.length);
+// 		// console.log(res.data.articles.bootstrap.length);
+// 		// console.log(res.data.articles.bootstrap[0]);
+
+// 		console.log(Object.keys(res.data.articles));
+
+// 		Object.keys(res.data.articles).forEach((elem) => {
+// 			console.log(Object.keys(res.data.articles)[elem]);
+// 		});
+
+// 		let obj = { first: 'hi' };
+// 		console.log(obj[Object.keys(obj)[0]]); //returns hi
+
+// 		console.log('--------------');
+
+// 		// res.data.articles[Object.keys(res.data.articles)[0]]
+// 		console.log(res.data.articles[Object.keys(res.data.articles)[0]]);
+// 		console.log(
+// 			res.data.articles[
+// 				Object.keys(res.data.articles)[[Object.keys(res.data.articles)[0]]]
+// 			]
+// 		);
+
+// 		// res.data.articles[Object.keys(res.data.articles)[0]]
+
+// 		let articles = res.data.articles;
+// 		let articleCats = [];
+// 		Object.keys(articles).forEach((elem) => {
+// 			articleCats.push(elem);
+// 		});
+// 		console.log('article titles -> ', articleCats);
+
+// 		let articleTitles = [];
+// 		articleCats.forEach((elem) => {
+// 			console.log(articles[elem]);
+// 			articleTitles.push(articles[elem].headline);
+// 		});
+// 		console.log('articleTitles -> ', articleTitles);
+
+// 		// Object.keys(articles)[0]
+// 		// console.log(Object.keys(articles)[0]);
+// 	})
+// 	.catch((err) => {
+// 		console.log(err);
+// 	});
 
 function cardMaker({ headline, authorPhoto, authorName }) {
 	//instantiate
@@ -95,17 +112,20 @@ function cardMaker({ headline, authorPhoto, authorName }) {
 	const author = document.createElement('span');
 
 	//class names
-	// tab.classList.add('tab');
 	card.classList.add('card');
 	articleTitle.classList.add('headline');
 	authorDiv.classList.add('author');
 	imgContainer.classList.add('img-container');
 
 	//text content
-	// headline.textContent = article.headline;
-	// console.log(headline);
+	articleTitle.textContent = headline;
+	img.src = authorPhoto;
+	author.textContent = authorName;
 
-	//listenr
+	//listener
+	card.addEventListener('click', () => {
+		console.log(articleTitle);
+	});
 
 	//return
 	return card;
